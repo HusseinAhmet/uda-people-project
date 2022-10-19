483891c5c512691ebf8220032e8d293f0ee18119
Hussein Ahmet <7sin99@gmail.com>
2022-10-19 02:56:31 +0200

    22222

--- .circleci/url.sh
+++ .circleci/url.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo $(aws ec2 describe-instances --filters "Name=tag:Name,Values=backend-$1" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text) >> .circleci/ansible/inventory.txt
+echo $(aws ec2 describe-instances --filters "Name=tag:Name,Values=backend-$1" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text) >> /ansible/inventory.txt
 
