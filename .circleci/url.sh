6bc5187e338742dde180df6d7cd012c6f6fb0080
Hussein Ahmet <7sin99@gmail.com>
2022-10-24 04:03:47 +0200

    smoke test 0

--- .circleci/url.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-echo $1
-aws ec2 describe-instances --filters "Name=tag:Name,Values=backend-$1" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text >> ~/project/.circleci/ansible/inventory.txt
-
